aff944ed575572308729fbb1de46fd738382a0a7
preludeinc <j.guenter@protonmail.com>
2025-01-19 23:21:28 -0800

    added start of login functionality

--- axum/src/main.rs
+++ axum/src/main.rs
@@ -1,25 +1,45 @@
 #![allow(unused)]
+mod error;
+mod web;
+
+pub use self::error::{Error, Result};
+
 use std::net::SocketAddr;
 use axum::extract::{Path, Query};
 use axum::response::{Html,IntoResponse};
-use axum::{routing::get,Router};
+use axum::routing::{get, get_service};
+use axum::{Router};
 
 use tokio::net::TcpListener;
+use tower_http::services::ServeDir;
 use serde::Deserialize;
 
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new()
-        .route("/hello", get(handler_hello))
-        .route("/hello2/{name}", get(handler_hello2));
+    let routes_all = Router::new()
+    .merge(routes_hello())
+    .merge(web::routes_login::routes())
+    .fallback_service(routes_static());
 
     // region:      --- Start Server
     let addr = SocketAddr::from(([127,0,0,1], 8080));
     println!("--> LISTENING on {addr}\n");
 
     let tcp = TcpListener::bind(&addr).await.unwrap();
-    axum::serve(tcp, routes_hello).await.unwrap();
+    axum::serve(tcp, routes_all).await.unwrap();
     // endregion:   --- Start Server
+}
+
+fn routes_static() -> Router {
+    Router::new().fallback_service(get_service(ServeDir::new("./")))
+}
+
+// region:      -- Routes Hello
+fn routes_hello() -> Router {
+    let routes_hello = Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/{name}", get(handler_hello2));
+    routes_hello
 }
 
 // region:      --- Handler Hello
--- axum/tests/quick_dev.rs
+++ axum/tests/quick_dev.rs
@@ -1,13 +1,27 @@
 #![allow(unused)]
 
 use anyhow::Result;
+use serde_json::json;
 
 #[tokio::test]
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080");
+    let hc2 = httpc_test::new_client("http://localhost:8080");
 
     //hc.expect("REASON").do_get("/hello?name=Jen").await?.print().await?;
-    hc.expect("REASON").do_get("/hello2/Mike").await?.print().await?;
+    //hc.expect("REASON").do_get("/hello2/Mike").await?.print().await?;
+
+    hc.expect("MAIN").do_get("/src/main.rs").await?.print().await?;
+
+    let binding = hc2.expect("LOGIN");
+    let req_login = binding.do_post(
+        "/api/login",
+        json!({
+            "username": "demo1",
+            "pwd": "welcome"
+        })
+    );
+    req_login.await?.print().await?;
 
     Ok(())
 }
