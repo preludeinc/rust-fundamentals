c831cecd67f6634274412581d0bd759d3153aa26
preludeinc <j.guenter@protonmail.com>
2025-01-19 16:51:07 -0800

    added handler_hello2

--- axum/src/main.rs
+++ axum/src/main.rs
@@ -1,32 +1,17 @@
 #![allow(unused)]
 use std::net::SocketAddr;
-use axum::{
-    extract::Query,
-    response::IntoResponse,
-    response::Html,
-    routing::get,
-    Router,
-    };
+use axum::extract::{Path, Query};
+use axum::response::{Html,IntoResponse};
+use axum::{routing::get,Router};
+
 use tokio::net::TcpListener;
 use serde::Deserialize;
 
-// region:      --- Handler Hello
-#[derive(Debug, Deserialize)]
-struct HelloParams {
-    name: Option<String>,
-}
-
-async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
-    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
-    
-    let name = params.name.as_deref().unwrap_or("World!");
-    Html(format!("Hello <strong>{name}</strong>"))
-}
-// endregion:   --- Handler Hello
-
 #[tokio::main]
 async fn main() {
-    let routes_hello = Router::new().route("/hello", get(handler_hello));
+    let routes_hello = Router::new()
+        .route("/hello", get(handler_hello))
+        .route("/hello2/{name}", get(handler_hello2));
 
     // region:      --- Start Server
     let addr = SocketAddr::from(([127,0,0,1], 8080));
@@ -36,4 +21,26 @@ async fn main() {
     axum::serve(tcp, routes_hello).await.unwrap();
     // endregion:   --- Start Server
 }
+
+// region:      --- Handler Hello
+#[derive(Debug, Deserialize)]
+struct HelloParams {
+    name: Option<String>,
+}
+
+// ex: `/hello?name=Jen`
+async fn handler_hello(Query(params): Query<HelloParams>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello - {params:?}", "HANDLER");
+    
+    let name = params.name.as_deref().unwrap_or("World!");
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+
+// ex: `/hello2/Mike`
+async fn handler_hello2(Path(name): Path<String>) -> impl IntoResponse {
+    println!("->> {:<12} - handler_hello2 - {name:?}", "HANDLER");
+
+    Html(format!("Hello <strong>{name}</strong>"))
+}
+// endregion:   --- Handler Hello
 
--- axum/tests/quick_dev.rs
+++ axum/tests/quick_dev.rs
@@ -6,7 +6,8 @@ use anyhow::Result;
 async fn quick_dev() -> Result<()> {
     let hc = httpc_test::new_client("http://localhost:8080");
 
-    hc.expect("REASON").do_get("/hello?name=Jen").await?.print().await?;
+    //hc.expect("REASON").do_get("/hello?name=Jen").await?.print().await?;
+    hc.expect("REASON").do_get("/hello2/Mike").await?.print().await?;
 
     Ok(())
 }
