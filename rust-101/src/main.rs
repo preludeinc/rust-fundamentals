26c27adce6cb9348a87ed3c6463178b4b79bb48e
preludeinc <j.guenter@protonmail.com>
2025-01-21 14:02:34 -0800

    added part07

--- rust-101/src/main.rs
+++ rust-101/src/main.rs
@@ -1,65 +1,74 @@
 /*
 * Continuation of BigInt class.
+* Featuring operator Overloading.
 *
 */
 mod part05;
 use part05::BigInt;
+use std::fmt;
 
-impl BigInt {
-    fn min_try1(self, other: Self) -> Self {
-        debug_assert!(self.test_invariant() && other.test_invariant());
-    
-        if self.data.len() < other.data.len() {
-            self
-        } else if self.data.len() > other.data.len() {
-            other
-        } else {
-            if self.data > other.data {
-                self
-            }
-            else {
-                other
-            }
-        }
-    }
+pub trait Minimum {
+    fn min<'a>(&'a self, other: &'a Self) -> &'a Self;
 }
 
-fn vec_min(v: &Vec<BigInt>) ->Option<BigInt> {
-    let mut min: Option<BigInt> = None;
-
+pub fn vec_min<T: Minimum>(v: &Vec<T>) -> Option<&T> {
+    let mut min: Option<&T> = None;
     for e in v {
-        let e = e.clone();
         min = Some(match min {
             None => e,
-            Some(n) => e.min_try1(n)
+            Some(n) => n.min(e)
         });
     }
     min
 }
 
-use part05::SomethingOrNothing;
-impl<T: Copy> Copy for SomethingOrNothing<T>{}
-
-fn head<T>(v: &Vec<T>) -> Option<&T> {
-    if v.len() > 0 {
-        Some(&v[0])
-    } else {
-        None
+impl Minimum for BigInt {
+    fn min<'a>(&'a self, other: &'a Self) -> &'a Self {
+        if self.data.len() < other.data.len() {
+            self
+        } else if self.data.len() > other.data.len() {
+            other
+        } else if self.data < other.data {
+            self
+        } else {
+            other
+        }
     }
 }
 
-// used for a demonstration in this section
-// fn rust_foo(mut v: Vec<i32>) -> i32 {
-//     let first: Option<&i32> = head(&v);
-//     /* v.push(42); */
-//     *first.unwrap()
-// }
-
-fn main() {
-    let v1 = BigInt::new(6 << 16);
-    let v2 = BigInt::new(1 << 16);
-    println!("{:?}", v1);
-    println!("{:?}", v2);
-    let b1 = BigInt::min_try1(v1, v2);
-    println!("{:?}", b1);
+impl PartialEq for BigInt {
+    #[inline]
+    fn eq(&self, other: &BigInt) -> bool {
+        debug_assert!(self.test_invariant() && other.test_invariant());
+        self.data == other.data
+    }
 }
+
+impl fmt::Debug for BigInt {
+    fn fmt(&self, f: &mut fmt::Formatter) -> fmt::Result {
+        self.data.fmt(f)
+    }
+}
+
+fn compare_big_ints() {
+    let b1 = BigInt::new(13);
+    let b2 = BigInt::new(37);
+    println!("b1 == b1: {} ; b1 == b2: {}; b1 != b2: {}", b1 == b1, b1 == b2, b1 != b2);
+}
+
+#[test]
+fn test_min() {
+    let b1 = BigInt::new(1);
+    let b2 = BigInt::new(42);
+    let b3 = BigInt::from_vec(vec![0, 1]);
+
+    assert!(*b1.min(&b2) == b1);
+    assert!(*b3.min(&b2) == b2);
+
+    let v1 = vec![b2.clone(), b1.clone(), b3.clone()];
+    let v2 = vec![b2.clone(), b3.clone()];
+
+    assert_eq!(vec_min(&v1), Some(&b1));
+    assert_eq!(vec_min(&v2), Some(&b2));
+}
+
